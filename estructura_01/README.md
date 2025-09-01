b6e6e4a7c25e8c044c04506601199e3c8e68717a
JuanAzara <juane.azara@gmail.com>
2025-08-31 23:05:21 -0400

    Se agrega codigo de prueba

---
+++ estructura_01/README.md
@@ -0,0 +1,3 @@
+# estructura_01
+
+A new Flutter project.
---
+++ estructura_01/lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          leading: const Icon(Icons.menu),
+          title: const Text('Diagramación'),
+          actions: const [
+            Icon(Icons.more_vert),
+          ],
+        ),
+        body: ListView(
+          children: [
+            ListTile(
+              leading: const CircleAvatar(
+                backgroundColor: Color.fromARGB(255, 229, 14, 14),
+              ),
+              title: const Text('Item 1'),
+              trailing: ElevatedButton(
+                onPressed: () {},
+                child: const Text('Acción'),
+              ),
+            ),
+            const Divider(),
+            ListTile(
+              leading: const CircleAvatar(
+                backgroundColor: Color.fromARGB(255, 175, 76, 135),
+              ),
+              title: const Text('Item 2'),
+              trailing: ElevatedButton(
+                onPressed: () {},
+                child: const Text('Acción'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
